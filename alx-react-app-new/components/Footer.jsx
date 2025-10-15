525cf5688498e427056406f2856693fb5ed47fbd
mohamed55J <k40567355@gmail.com>
2025-10-15 02:13:09 +0100

    Add files via upload

---
+++ alx-react-app-new/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+  const footerStyle = {
+    backgroundColor: "#023e8a",
+    color: "white",
+    textAlign: "center",
+    padding: "10px",
+    marginTop: "20px",
+    borderRadius: "8px"
+  };
+
+  return (
+    <footer style={footerStyle}>
+      <p>2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
