8de44a12bcc7c7e63cb2ef0e14e9d6372b43564e
mohamed55J <k40567355@gmail.com>
2025-10-15 13:33:12 +0100

    Add files via upload

---
+++ alx-react-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2025 City Lovers</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ alx-react-app/src/components/WelcomeMessage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Welcome to ALX React App</h1>
+      <p>This is a simple JSX component.</p>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
